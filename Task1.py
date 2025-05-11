a30c533100c4d238223b5ba2fcf3f044c69ad898
Paras-Shethia <parashethia@gmail.com>
2025-05-11 18:31:10 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,6 @@
+f=5
+s=10
+print("Addition:",f+s)
+print("Subtraction:",f-s)
+print("Multiplication:",f*s)
+print("Division:",f/s)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+fname=input("First Name:")
+sname=input("Last Name:")
+print("Hello,",fname,sname,"welcome to Python Program")
