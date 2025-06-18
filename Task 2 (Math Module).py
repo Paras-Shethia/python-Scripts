9c5ba24704a5ec264291f4cd238ca0f7e1b6a1c6
Paras-Shethia <parashethia@gmail.com>
2025-06-18 19:28:08 +0530

    Update Task 2 (Math Module).py

--- Task 2 (Math Module).py
+++ Task 2 (Math Module).py
@@ -1,7 +1,7 @@
-# num=int(input("Enter a Number: "))
-# import math
-# a = math.sqrt(num)
-# b = math.log(num)
-# c = math.sin(num)
-# print(f"Square Root: {a}\nLogarithm: {b}\nSine: {c}")
+num=int(input("Enter a Number: "))
+import math
+a = math.sqrt(num)
+b = math.log(num)
+c = math.sin(num)
+print(f"Square Root: {a}\nLogarithm: {b}\nSine: {c}")
  
